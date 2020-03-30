5d4b33ad0ac3350b1c221a4788aa4f35309c1ce3
free-2-fly <wulong.sky@gmail.com>
2020-03-30 18:08:37 -0400

    Initialize firebase and create navigation

--- App.js
+++ App.js
@@ -1,19 +1,48 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import {
+  createStackNavigation,
+  createStackNavigator
+} from "react-navigation-stack";
+import LoadingScreen from "./screens/LoadingScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import LoginScreen from "./screens/LoginScreen";
+import HomeScreen from "./screens/HomeScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import * as firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyD8L8vYBLk0TpLnN-aAC9gS1-m3TmAIuf0",
+  authDomain: "apptrack-a3d34.firebaseapp.com",
+  databaseURL: "https://apptrack-a3d34.firebaseio.com",
+  projectId: "apptrack-a3d34",
+  storageBucket: "apptrack-a3d34.appspot.com",
+  messagingSenderId: "230145792266",
+  appId: "1:230145792266:web:96cdf7f808ba8f908982f3",
+  measurementId: "G-DR8GT8LG1B"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const AppStack = createStackNavigator({
+  Home: HomeScreen
 });
+
+const AuthStack = createStackNavigator({
+  Register: RegisterScreen,
+  Login: LoginScreen
+});
+
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      Loading: LoadingScreen,
+      Auth: AuthStack,
+      App: AppStack
+    },
+    {
+      initialRouteName: "Loading"
+    }
+  )
+);

92dc4ebf60d33e92770b6fbf2024cc0d97384510
free-2-fly <wulong.sky@gmail.com>
2020-04-02 18:47:08 -0400

    Add bottom tab navigation with icons

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
+import React from "react";
+
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
-import {
-  createStackNavigation,
-  createStackNavigator
-} from "react-navigation-stack";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { Ionicons } from "@expo/vector-icons";
 import LoadingScreen from "./screens/LoadingScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import LoginScreen from "./screens/LoginScreen";
 import HomeScreen from "./screens/HomeScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import AddJobScreen from "./screens/AddJobScreen";
 
 import * as firebase from "firebase";
 
@@ -25,21 +28,53 @@ if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
-const AppStack = createStackNavigator({
-  Home: HomeScreen
-});
+const AppTabNavigator = createBottomTabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="ios-home" size={24} color={tintColor} />
+        )
+      }
+    },
+    "Add Job": {
+      screen: AddJobScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="ios-add-circle" size={24} color={tintColor} />
+        )
+      }
+    },
+    Profile: {
+      screen: ProfileScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="ios-person" size={24} color={tintColor} />
+        )
+      }
+    }
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: "#5271FF",
+      inactiveTintColor: "#B8BBC4",
+      showLabel: false
+    }
+  }
+);
 
 const AuthStack = createStackNavigator({
-  Register: RegisterScreen,
-  Login: LoginScreen
+  Login: LoginScreen,
+  Register: RegisterScreen
 });
 
 export default createAppContainer(
   createSwitchNavigator(
     {
       Loading: LoadingScreen,
-      Auth: AuthStack,
-      App: AppStack
+      App: AppTabNavigator,
+      Auth: AuthStack
     },
     {
       initialRouteName: "Loading"

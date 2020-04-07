e5ad2f0323a4670e6a706d4bb4cd7e8ec24fc6cb
free-2-fly <wulong.sky@gmail.com>
2020-04-07 12:59:10 -0400

    Add getJobs function to get jobs of current user on screen mount

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,26 +1,42 @@
 import React, { useState, useEffect } from "react";
 
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import * as firebase from "firebase";
+import * as firebase from "firebase/app";
+import "firebase/firestore";
 
 export default function HomeScreen() {
-  const [email, setEmail] = useState("");
   const [displayName, setDisplayName] = useState("");
+  const [jobs, setJobs] = useState([]);
 
   useEffect(() => {
-    const { email, displayName } = firebase.auth().currentUser;
-
-    setEmail(email);
+    const { displayName } = firebase.auth().currentUser;
     setDisplayName(displayName);
+    getJobs();
   }, []);
 
   const logOutUser = () => {
     firebase.auth().signOut();
   };
 
+  const getJobs = () => {
+    let jobData = [];
+    let userId = (firebase.auth().currentUser || {}).uid;
+    firebase
+      .firestore()
+      .collection("jobs")
+      .where("uid", "==", userId)
+      .get()
+      .then((snapshot) => {
+        snapshot.forEach((doc) => {
+          jobData.push(doc.data());
+        });
+        setJobs(jobData);
+      });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Hello {email}!</Text>
+      <Text>Hello {displayName}!</Text>
 
       <TouchableOpacity onPress={logOutUser}>
         <Text>Logout</Text>
@@ -33,6 +49,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
-  }
+    alignItems: "center",
+  },
 });

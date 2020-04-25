5eb7e94d8f07a3fbbf30868d73f86ff8d125393a
free-2-fly <wulong.sky@gmail.com>
2020-04-25 18:51:21 -0400

    Add user account info

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,18 +1,50 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import Header from "../components/Header";
+import { connect } from "react-redux";
+import * as firebase from "firebase/app";
 
-export default function ProfileScreen() {
+function ProfileScreen(props) {
+  const userCreationDate = (
+    firebase.auth().currentUser || {}
+  ).metadata.creationTime
+    .split(" ")
+    .splice(1, 3)
+    .join(" ");
+
+  const userEmail = (firebase.auth().currentUser || {}).email;
+
   return (
     <View style={styles.container}>
-      <Text>Profile Screen</Text>
+      <Text style={styles.jobsAdded}>Email</Text>
+      <Text style={styles.jobsAdded}>{userEmail}</Text>
+
+      <Text style={styles.jobsAdded}>Registered on</Text>
+      <Text style={styles.jobsAdded}>{userCreationDate}</Text>
+
+      <Text style={styles.jobsAdded}>Jobs Added</Text>
+      <Text style={styles.jobsAdded}>{props.jobs.length}</Text>
+
+      <Header />
     </View>
   );
 }
 
+const mapStateToProps = (state) => {
+  return {
+    jobs: state.jobs,
+  };
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  }
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  jobsAdded: {
+    fontSize: 25,
+  },
 });
+
+export default connect(mapStateToProps, null)(ProfileScreen);

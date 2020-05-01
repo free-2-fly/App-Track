926f197faeac616e0a7c54004c55c82170658fd6
free-2-fly <wulong.sky@gmail.com>
2020-05-01 17:17:57 -0400

    Delete unused actions

--- redux/actions/user.js
+++ redux/actions/user.js
@@ -1,24 +1,8 @@
 export const SET_USER = "SET_USER";
-export const SET_USERNAME = "SET_USERNAME";
-export const SET_REGISTERED_USER = "SET_REGISTERED_USER";
 
 export const setUser = (user) => {
   return {
     type: "SET_USER",
     payload: user,
-  };
-};
-
-export const setUsername = (user) => {
-  return {
-    type: "SET_USERNAME",
-    payload: user,
-  };
-};
-
-export const setRegisteredUser = () => {
-  return (dispatch, getState) => {
-    const { user } = getState().user;
-    return dispatch({ type: SET_REGISTERED_USER, payload: user });
   };
 };

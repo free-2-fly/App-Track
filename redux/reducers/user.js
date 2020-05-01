89738864f21c80decade26a19a0f655a89daf21a
free-2-fly <wulong.sky@gmail.com>
2020-05-01 17:19:10 -0400

    Delete unused cases

--- redux/reducers/user.js
+++ redux/reducers/user.js
@@ -1,4 +1,4 @@
-import { SET_USER, SET_USERNAME, SET_REGISTERED_USER } from "../actions/user";
+import { SET_USER } from "../actions/user";
 
 const initialState = {
   user: null,
@@ -9,10 +9,6 @@ const initialState = {
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_USER:
-      return { ...state, user: action.payload };
-    case SET_USERNAME:
-      return { ...state, user: action.payload };
-    case SET_REGISTERED_USER:
       return { ...state, user: action.payload };
     default:
       return state;

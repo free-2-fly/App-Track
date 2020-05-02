293dc95a0c3b6c23b56567c3552f5196d2408dd9
free-2-fly <wulong.sky@gmail.com>
2020-05-02 11:18:34 -0400

    Add account icon to header

--- components/Header.js
+++ components/Header.js
@@ -4,6 +4,7 @@ import * as firebase from "firebase/app";
 import { SimpleLineIcons } from "@expo/vector-icons";
 import { TouchableOpacity } from "react-native";
 import { connect } from "react-redux";
+import { MaterialIcons } from "@expo/vector-icons";
 
 function Header(props) {
   const logOutUser = () => {
@@ -11,11 +12,22 @@ function Header(props) {
   };
 
   return (
-    <View style={styles.header}>
-      <Text style={styles.greetingMessage}>Hello, {props.user}!</Text>
-      <TouchableOpacity onPress={logOutUser} style={{ left: 290, top: 72 }}>
-        <SimpleLineIcons name="logout" size={40} style={{ color: "#fefefe" }} />
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <MaterialIcons
+          name="account-circle"
+          size={40}
+          style={styles.accountIcon}
+        />
+        <Text style={styles.username}>{props.user}</Text>
+        <TouchableOpacity onPress={logOutUser} style={styles.logOutButton}>
+          <SimpleLineIcons
+            name="logout"
+            size={33}
+            style={{ color: "#fefefe" }}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -27,7 +39,7 @@ const mapStateToProps = ({ userReducer: { user } }) => {
 };
 
 const styles = StyleSheet.create({
-  header: {
+  container: {
     backgroundColor: "#494E58",
     borderBottomLeftRadius: 90,
     borderTopLeftRadius: 0,
@@ -38,14 +50,23 @@ const styles = StyleSheet.create({
     top: 0,
     width: "100%",
   },
-  greetingMessage: {
-    borderBottomLeftRadius: 90,
+  username: {
     color: "#fefefe",
     fontSize: 30,
     fontWeight: "500",
+    marginLeft: 80,
+  },
+  accountIcon: {
+    color: "#fefefe",
     position: "absolute",
-    right: "35%",
-    top: "50%",
+    left: 30,
+  },
+  header: {
+    marginTop: 70,
+  },
+  logOutButton: {
+    position: "absolute",
+    right: 30,
   },
 });
 
